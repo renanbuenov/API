03691b3c0480322ee5fd32d93743b19652301c52
Renan Bueno <renanbuenov@hotmail.com>
2021-11-01 18:40:35 -0300

    Exception for email already registered

--- src/main/java/br/com/renan/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/renan/api/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.renan.api.domain.User;
 import br.com.renan.api.domain.dto.UserDTO;
 import br.com.renan.api.repositories.UserRepository;
 import br.com.renan.api.services.UserService;
+import br.com.renan.api.services.exceptions.DataIntegrityViolationException;
 import br.com.renan.api.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return repository.save(mapper.map(obj, User.class));
     }
+
+    private void findByEmail(UserDTO obj) {
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent()) {
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
+        }
+    }
+
 }
