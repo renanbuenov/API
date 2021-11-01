a2098ec1a1338523801eed7177c3edbfa8975d09
Renan Bueno <renanbuenov@hotmail.com>
2021-10-31 22:20:50 -0300

    Class user service impl findById

---
+++ src/main/java/br/com/renan/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.renan.api.services.impl;
+
+import br.com.renan.api.domain.User;
+import br.com.renan.api.repositories.UserRepository;
+import br.com.renan.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
