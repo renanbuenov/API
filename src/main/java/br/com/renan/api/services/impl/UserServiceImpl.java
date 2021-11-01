27982922b66243c9955f88a21f79ef69d040441c
Renan Bueno <renanbuenov@hotmail.com>
2021-11-01 18:28:21 -0300

    Enpoint create for user

--- src/main/java/br/com/renan/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/renan/api/services/impl/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package br.com.renan.api.services.impl;
 
 import br.com.renan.api.domain.User;
+import br.com.renan.api.domain.dto.UserDTO;
 import br.com.renan.api.repositories.UserRepository;
 import br.com.renan.api.services.UserService;
 import br.com.renan.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +18,21 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 }
