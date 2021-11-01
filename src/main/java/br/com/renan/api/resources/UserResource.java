2100388309290b3a23c5a0fc921b885847816b57
Renan Bueno <renanbuenov@hotmail.com>
2021-10-31 22:21:09 -0300

    Class user resource findById

--- src/main/java/br/com/renan/api/resources/UserResource.java
+++ src/main/java/br/com/renan/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.renan.api.resources;
 
 import br.com.renan.api.domain.User;
+import br.com.renan.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-
-        return ResponseEntity.ok().body(new User(1, "Renan", "renan@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 }
