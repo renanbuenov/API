490e13016c954dfdf4e0fc8d74cb50a61da0f845
Renan Bueno <renanbuenov@hotmail.com>
2021-11-03 10:35:43 -0300

    Fixed bug

--- src/main/java/br/com/renan/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/renan/api/domain/dto/UserDTO.java
@@ -3,11 +3,12 @@ package br.com.renan.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
